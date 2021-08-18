904633801b00e7d4edf485078e8f7ca54491dc25
Michael Canfield <mikecan@cisco.com>
2021-08-18 15:50:46 -0700

    feat: add new game images

--- apps/api/src/app/games.repository.ts
+++ apps/api/src/app/games.repository.ts
@@ -4,7 +4,7 @@ const games: Game[] = [
   {
     id: 'settlers-in-the-can',
     name: 'Settlers in the Can',
-    image: '/assets/beans.png', // 'https://media.giphy.com/media/xUNda3pLJEsg4Nedji/giphy.gif',
+    image: '/assets/oscar.gif', // 'https://media.giphy.com/media/xUNda3pLJEsg4Nedji/giphy.gif',
     description:
       'Help your bug family claim the best real estate in a spilled can of beans.',
     price: 35,
@@ -13,7 +13,7 @@ const games: Game[] = [
   {
     id: 'chess-pie',
     name: 'Chess Pie',
-    image: '/assets/chess.png', // 'https://media.giphy.com/media/iCZyBnPBLr0dy/giphy.gif',
+    image: '/assets/chess.gif', // 'https://media.giphy.com/media/iCZyBnPBLr0dy/giphy.gif',
     description: 'A circular game of Chess that you can eat as you play.',
     price: 15,
     rating: Math.random(),
@@ -21,7 +21,7 @@ const games: Game[] = [
   {
     id: 'purrfection',
     name: 'Purrfection',
-    image: '/assets/cat.png', // 'https://media.giphy.com/media/12xMvwvQXJNx0k/giphy.gif',
+    image: '/assets/cat.gif', // 'https://media.giphy.com/media/12xMvwvQXJNx0k/giphy.gif',
     description: 'A cat grooming contest goes horribly wrong.',
     price: 45,
     rating: Math.random(),
